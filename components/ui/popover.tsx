d37ea0e8d32d18f65898aa4abc8a865a8ec42158
Tim <tas57atx@gmail.com>
2024-09-04 11:17:27 -0500

    fixed error with popover

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -5,14 +5,14 @@ import * as PopoverPrimitive from '@radix-ui/react-popover';
 import { cn } from '@/lib/utils';
 
 const Popover =  PopoverPrimitive.Root
-const PopoverTrigger = PopoverPrimitive.SheetTrigger
+const PopoverTrigger = PopoverPrimitive.Trigger
 
 const PopoverContent = React.forwardRef<
     React.ElementRef<typeof PopoverPrimitive.Content>,
     React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
   >(({ className, align ='center', sideOffset = 4, ...props }, ref) =>(
     <PopoverPrimitive.Portal>
-        <PopoverPrimitive.SheetContent
+        <PopoverPrimitive.Content
             ref={ref}
             align={align}
             sideOffset={sideOffset}
